8e341517ec349c34e899768c340b52c9c082c062
Alex Reichert <reichertjalex@gmail.com>
2016-05-19 00:45:42 -0700

    Trying to send message through fb api

--- server/controllers/fb.js
+++ server/controllers/fb.js
@@ -1,4 +1,8 @@
+import fetch from 'isomorphic-fetch';
+
 const VERIFY_TOKEN = process.env.VERIFY_TOKEN
+const PAGE_TOKEN = process.env.PAGE_TOKEN
+const MESSAGES_PATH = 'https://graph.facebook.com/v2.6/me/messages'
 
 export function verify (req, res) {
   console.log('Request query:', req.query)
@@ -11,19 +15,46 @@ export function verify (req, res) {
 }
 
 export function listen (req, res) {
-  var messaging_events = req.body.entry[0].messaging
+  const events = req.body.entry[0].messaging
 
-  for (var i = 0; i < messaging_events.length; i++) {
-    var event = req.body.entry[0].messaging[i]
-    var sender = event.sender.id
+  console.log('EVENTS!', events)
 
-    console.log(event)
-    if (event.message && event.message.text) {
-      var text = event.message.text
+  let promises = events
+    .filter(e => {
+      return (e.sender && e.sender.id) && (e.message && e.message.text)
+    })
+    .map(e => {
+      return textMessage({
+        id: e.sender.id,
+        text: e.message.text
+      })
+    })
 
-      console.log(text)
-    }
+  Promise.all(promises)
+    .then(() => res.sendStatus(200))
+    .catch(err => {
+      console.log('ERROR!', err);
+      res.sendStatus(500);
+    })
+}
+
+function textMessage(id, text) {
+  const message = {
+    recipient: { id },
+    message: { text }
   }
 
-  res.sendStatus(200)
-}
+  const json = JSON.stringify(message);
+
+  console.log('Attempting to send message:', message)
+  console.log('To path: ', `${MESSAGES_PATH}?access_token=${PAGE_TOKEN}`)
+
+  return fetch(`${MESSAGES_PATH}?access_token=${PAGE_TOKEN}`, {
+    method: 'POST',
+    headers: {
+     'Accept': 'application/json',
+     'Content-Type': 'application/json',
+    },
+    body: json
+  });
+
